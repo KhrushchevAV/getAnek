d8a98da577b1c3973df7a6720c81bf7bda1cb4be
Andrey Khrushchev <ahrusch@mail.ru>
2015-10-01 20:18:50 +0500

    Initial state. 2013-10-15. upb.

---
+++ getAnek.pl
@@ -0,0 +1,107 @@
+#!/usr/bin/perl -w
+#
+#  windows 1251 use for regexp !!!
+#
+# 2009-12-21 опять поменяли формат html
+# 2010-06-30 смена формата. не стало <pre>
+# 2013-01-10 опять поменяли формат. в 1 строку все анекдоты. :-(
+
+use LWP::UserAgent;
+use HTTP::Request;
+#---
+my($url) = 'http://www.anekdot.ru/last/j.html';
+my(@html);
+my($pr) = 0;
+my($recip) = '<nvimc@mail.ru>, <sed@upb.ru>, <urasov@upb.ru>, <vedenina@upb.ru>, <ermakov@ipae.uran.ru>, <NetSveta@upb.ru>, <gaz@upb.ru>, <grishins@inbox.ru>, <slava@upb.ru>, <av@upb.ru>';
+#my($recip) = '<av@upb.ru>';
+
+
+#-----------------
+if (GetHtml())
+{
+  open(HTML,"<anek.html") || die "can't open anek.html\n";
+  open(RES,">a.txt");
+  print RES "To: $recip\n";
+  print RES "Subject: :-)\n";
+  print RES "From: av\@upb.ru\n";
+  print RES "MIME-Version: 1.0\n";
+  print RES "Content-Transfer-Encoding: 8bit\n";
+  print RES 'Content-Type: text/plain; charset="Windows-1251"' . "\n\n";
+  while(<HTML>)
+  {
+    if (/<pre>(.*)/)  
+    {
+      $pr = 1; 
+      $_ = $1;
+    }
+#  <div class="topictext">Вопреки советам военных никогда не закрывайте глаза и не падайте ногами<br />
+#  в сторону ядерного взрыва, ибо в любом случае вы видите это шоу первый и<br />последний раз.</div><br />
+    if (/<div class=.topictext.>(.*?)<\/div>/)
+    {
+      $_ = $1 . "\n\n\n"; 
+      s/<br.\/>/\n/gi;
+      print RES "$_";
+    }
+#<div class="text">Новое открытие сделали американские ученые: оказывается, iPhone 4<br />снижает содержание хлора в американской курятине до санитарных<br />стандартов России..</div>    
+    if (/<div class=.text.*?>(.*?)<\/div>/)
+    {
+      $_ = $1 . "\n\n\n"; 
+      s/<br.\/>/\n/gi;
+      print RES "$_";
+    }
+
+    if ($pr == 1)   
+    {
+      s/<INDEX>//;
+      s/<\/INDEX>//;
+      if (/(.*)<\/pre>/)
+      {
+        $pr = 0;
+        $_ = $1 . "\n\n\n"; 
+      }
+      print RES "$_";
+    }
+  }
+  close(RES);
+  close(HTML);
+}
+#system('c:\usr\lib\sendmail -t <a.txt');
+system('sendmail <a.txt');
+
+
+sub ParseOneStr {
+
+}
+
+
+#--------------------
+sub GetHtml
+{
+  $lwp = LWP::UserAgent->new;
+#  $lwp->proxy(['http','ftp'],'http://proxy:8080/');
+#  $lwp->credentials('','','up_bank\sys','bynthytnnhfabr');
+#  $r = HTTP::Request->new(GET => "$url");
+#  $r->proxy_authorization_basic('sys','bynthytnnhfabr');
+  $response = $lwp->request($r);
+  if ($response->is_success)
+  {
+    @html = $response->content;
+  }
+  else
+  {
+    @html = $response->error_as_HTML;
+  }
+
+  open(HTML,">anek.html") || die "can't open anek.html\n";
+
+  foreach (@html) {
+    s/<div/\n<div/gi;
+    print HTML $_;
+  }
+
+  #print HTML @html;
+  close(HTML);
+  return $response->is_success;
+}
+
+
